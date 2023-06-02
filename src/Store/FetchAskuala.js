539545a0b94d2030cc498471f45c7b6e28b7c23b
yonatan12121 <yonatanwork122@gmail.com>
2023-06-02 17:13:04 +0300

    fetch user

--- src/Store/FetchAskuala.js
+++ src/Store/FetchAskuala.js
@@ -1,6 +1,30 @@
+import axios from "axios";
+
 export const fetchUserLogin = async (data) => {
-  console.log(data);
-};
+  // console.log(data);
+  const email = data.email;
+  const password = data.password;
+  console.log(email,password);
+    await axios.post("http://localhost:5000/login-user", {
+      email,
+      password,
+      ContentType: "application/json",
+      Accept: "application/json",
+    });
+  };
+
 export const fetchUserRegister= async (data) => {
   console.log(data);
+  const FullName =data.FullName;
+  const email =data.email;
+  const password = data.password;
+  const cpassword = data.cPassword;
+  await axios.post("http://localhost:5000/register", {
+    email,
+    FullName,
+    password,
+    cpassword,
+    ContentType: "application/json",
+    Accept: "application/json",
+  });
 };
