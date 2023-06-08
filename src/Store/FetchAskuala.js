a4e357c4743a3023bd8197395402044abfac7e4d
beka-tegen <bekategene177@gmail.com>
2023-06-08 16:12:31 +0300

    modify

--- src/Store/FetchAskuala.js
+++ src/Store/FetchAskuala.js
@@ -1,31 +1,25 @@
 import axios from "axios";
 
-
 export const fetchUserLogin = async (data) => {
-  // console.log(data);
-  // const email = data.email;
-  // const password = data.password;
-  // console.log(email, password);
   const useData = await axios.post("http://localhost:5000/login-user", {
     data,
     ContentType: "application/json",
     Accept: "application/json",
-  })
-  console.log(useData, "login");
-  if (useData.data.role === 'student') {
+  });
+  if (useData.data.role === "Student") {
     window.location.href = "/student/dashboard";
+  }else if (useData.data.role === "Teacher") {
+    window.location.href = "/teacher/dashboard";
+  }else if (useData.data.role === "Supervisor") {
+    window.location.href = "/supervisor/dashboard";
+  }else if (useData.data.role === "Admin") {
+    window.location.href = "/admin/dashboard";
   }
   window.localStorage.setItem("token", useData.data.data);
   window.localStorage.setItem("id", useData.data.id);
   window.localStorage.setItem("gender", useData.data.role);
   window.localStorage.setItem("department", useData.data.department);
   window.localStorage.setItem("userName", useData.data.fullName);
-  // .then((data) => {
-
-  // var rool =data.data.role;
-  // if(rool === "student"){
-  //   window.location.href="/student/dashboard";
-  // }
 };
 
 export const fetchUserRegister = async (data) => {
@@ -36,13 +30,11 @@ export const fetchUserRegister = async (data) => {
     Accept: "application/json",
   });
 
-
   console.log(useData);
-  if (useData.data.status === 'ok') {
+  if (useData.data.status === "ok") {
     window.location.href = "/login";
   }
 };
-
 
 export const storebook = async (data) => {
   console.log(data);
@@ -52,19 +44,10 @@ export const storebook = async (data) => {
     Accept: "application/json",
   });
 
-
   console.log(useData);
-  if (useData.data.status === 'ok') {
-    // window.location.href = "/login"; redirect it to where ever you want 
+  if (useData.data.status === "ok") {
   }
 };
-// courseId
-// CourseName
-// Ects
-// creadit hr
-// lectureID
-// course dept
-
 
 export const storeCourse = async (data) => {
   console.log(data);
@@ -74,15 +57,10 @@ export const storeCourse = async (data) => {
     Accept: "application/json",
   });
 
-
   console.log(useData);
-  if (useData.data.status === 'ok') {
-    // window.location.href = "/login"; redirect it to where ever you want 
+  if (useData.data.status === "ok") {
   }
 };
-
-
-
 
 export const storeAnnouncement = async (data) => {
   console.log(data);
@@ -92,13 +70,11 @@ export const storeAnnouncement = async (data) => {
     Accept: "application/json",
   });
 
-
   console.log(useData);
-  if (useData.data.status === 'ok') {
-    // window.location.href = "/login"; redirect it to where ever you want 
+  if (useData.data.status === "ok") {
+    // window.location.href = "/login"; redirect it to where ever you want
   }
 };
-
 
 export const storeClass = async (data) => {
   console.log(data);
@@ -108,14 +84,11 @@ export const storeClass = async (data) => {
     Accept: "application/json",
   });
 
-
   console.log(useData);
-  if (useData.data.status === 'ok') {
-    // window.location.href = "/login"; redirect it to where ever you want 
+  if (useData.data.status === "ok") {
+    // window.location.href = "/login"; redirect it to where ever you want
   }
 };
-
-
 
 export const fetchTodoList = async (data) => {
   console.log(data);
@@ -125,36 +98,34 @@ export const fetchTodoList = async (data) => {
     Accept: "application/json",
   });
   console.log(TodoList);
-  if (TodoList.data.status === 'ok') {
-    // window.location.href = "/login"; redirect it to where ever you want 
-  } 
-
+  if (TodoList.data.status === "ok") {
+    // window.location.href = "/login"; redirect it to where ever you want
+  }
 };
-
 
 export const fetchBook = async () => {
   console.log("gsdsdgd");
   const useData = await axios.get("http://localhost:5000/fetchbook");
   console.log(useData);
-  return useData.data
+  return useData.data;
 };
 
 export const fetchClass = async () => {
   console.log("fetch class");
   const useData = await axios.get("http://localhost:5000/fetchclass");
   console.log(useData);
-  return useData.data
+  return useData.data;
 };
 
 export const fetchAnnouncement = async () => {
   console.log("announcement page");
   const useData = await axios.get("http://localhost:5000/fetchAnnouncement");
   console.log(useData);
-  return useData.data
+  return useData.data;
 };
 export const getTodoList = async () => {
   console.log("todolist page");
   const useData = await axios.get("http://localhost:5000/fetchTodo");
   console.log(useData);
-  return useData.data
+  return useData.data;
 };
