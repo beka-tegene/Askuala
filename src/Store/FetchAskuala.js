a533a87e0bce185156ed46acdeb2021fa64bbbad
yonatan12121 <yonatanwork122@gmail.com>
2023-06-09 14:43:44 +0300

    Course

--- src/Store/FetchAskuala.js
+++ src/Store/FetchAskuala.js
@@ -91,6 +91,25 @@ export const storeAnnouncement = async (data) => {
     ContentType: "application/json",
     Accept: "application/json",
   });
+  
+
+
+  console.log(useData);
+  if (useData.data.status === 'ok') {
+    // window.location.href = "/login"; redirect it to where ever you want 
+  }
+};
+
+
+
+export const storequestion = async (data) => {
+  console.log(data);
+  const useData = await axios.post("http://localhost:5000/storequestion", {
+    data,
+    ContentType: "application/json",
+    Accept: "application/json",
+  });
+  
 
 
   console.log(useData);
@@ -145,6 +164,14 @@ export const fetchClass = async () => {
   console.log(useData);
   return useData.data
 };
+
+export const fetchCourse = async () => {
+  console.log("fetch Course");
+  const useData = await axios.get("http://localhost:5000/fetchCourse");
+  console.log(useData);
+  return useData.data
+};
+
 
 export const fetchAnnouncement = async () => {
   console.log("announcement page");
