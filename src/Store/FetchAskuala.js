86e63f2517ba65a2c7a1bd9ce18c9b0912cca866
yonatan12121 <yonatanwork122@gmail.com>
2023-06-15 17:26:48 +0300

    role

--- src/Store/FetchAskuala.js
+++ src/Store/FetchAskuala.js
@@ -10,7 +10,7 @@ export const fetchUserLogin = async (data) => {
     window.location.href = "/student/dashboard";
   }else if (useData.data.role === "Teacher") {
     window.location.href = "/teacher/dashboard";
-  }else if (useData.data.role === "Supervisor") {
+  }else if (useData.data.role === "SuperVisor") {
     window.location.href = "/supervisor/dashboard";
   }else if (useData.data.role === "Admin") {
     window.location.href = "/admin/dashboard";
@@ -249,6 +249,7 @@ export const removeTodo = async (data) => {
   if (useData.data.status === "ok") {
     // window.location.href = "/login"; redirect it to where ever you want
   }
+  
 };
 
 
