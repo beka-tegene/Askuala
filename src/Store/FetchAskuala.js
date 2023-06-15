cb56ab4c7a951c4acfe22c642d98cc0fedc4784c
yonatan12121 <yonatanwork122@gmail.com>
2023-06-15 09:58:55 +0300

    todo

--- src/Store/FetchAskuala.js
+++ src/Store/FetchAskuala.js
@@ -102,6 +102,22 @@ export const storequestion = async (data) => {
   });
 
   
+
+  console.log(useData);
+  if (useData.data.status === "ok") {
+    // window.location.href = "/login"; redirect it to where ever you want
+  }
+};
+
+export const removeUser = async (data) => {
+  console.log(data);
+  const useData = await axios.post("http://localhost:5000/removeUser", {
+    data,
+    ContentType: "application/json",
+    Accept: "application/json",
+  });
+
+  
   
 
   console.log(useData);
@@ -110,7 +126,22 @@ export const storequestion = async (data) => {
   }
 };
 
+export const removeTodo = async (data) => {
+  console.log(data);
+  const useData = await axios.post("http://localhost:5000/removeTodo", {
+    data,
+    ContentType: "application/json",
+    Accept: "application/json",
+  });
 
+  
+  
+
+  console.log(useData);
+  if (useData.data.status === "ok") {
+    // window.location.href = "/login"; redirect it to where ever you want
+  }
+};
 
 export const storeJoin = async (data) => {
   console.log(data);
